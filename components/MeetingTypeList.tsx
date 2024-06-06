810144f7db9a826a44e4913d09840299a3120da7
Ali Hassan <alha2207@student.miun.se>
2024-06-06 16:41:57 +0200

    TESTING

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -73,28 +73,28 @@ const MeetingTypeList = () => {
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start an instant meeting"
+        title="Direkt möte"
+        description="Skapa direkt möte"
         handleClick={() => setMeetingState('isInstantMeeting')}
       />
       <HomeCard
         img="/icons/join-meeting.svg"
-        title="Join Meeting"
-        description="via invitation link"
+        title="Anslut till möte"
+        description="Anslut via länk"
         className="bg-blue-1"
         handleClick={() => setMeetingState('isJoiningMeeting')}
       />
       <HomeCard
         img="/icons/schedule.svg"
-        title="Schedule Meeting"
-        description="Plan your meeting"
+        title="Tidsboka möte"
+        description="Boka ditt möte"
         className="bg-purple-1"
         handleClick={() => setMeetingState('isScheduleMeeting')}
       />
       <HomeCard
         img="/icons/recordings.svg"
-        title="View Recordings"
-        description="Meeting Recordings"
+        title="Se inspelning"
+        description="Inspelningar"
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
       />
